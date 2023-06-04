74c5f2bdb706a1ebaf210c8b1ff0fbb8e0d613c5
karo-fox <karolina.kozubik@gmail.com>
2023-06-04 14:03:42 +0200

    add entrance map, update loading and saving

--- poketechnika/Tile.cpp
+++ poketechnika/Tile.cpp
@@ -10,7 +10,7 @@ const sf::Vector2f SCALE{ 2, 2 };
 
 std::map<tileTypes, std::string> textures{
 	{tileTypes::GRASS, std::string{TEXTURES_DIR + "/grass_tile.png"}},
-	{tileTypes::PAVEMENT, std::string{TEXTURES_DIR + "/pavement_tile.png"}},
+	{tileTypes::PAVEMENT, std::string{TEXTURES_DIR +  "/pavement_tile.png"}},
 	{tileTypes::STREET, std::string{TEXTURES_DIR + "/street_tile.png"}},
 	{tileTypes::PATH, std::string{TEXTURES_DIR + "/path_tile.png"}},
 	{tileTypes::BUILDING, std::string{TEXTURES_DIR + "/building_tile.png"}},
@@ -80,8 +80,6 @@ Tile tile_from_xml(pugi::xml_node& tile_node, int x, int y)
 		x = std::stoi(tile_node.child("position").child("x").child_value());
 		y = std::stoi(tile_node.child("position").child("y").child_value());
 	}
-	x *= 64;
-	y *= 64;
 
 	tileTypes type = static_cast<tileTypes>(std::stoi(tile_node.child("type").child_value()));
 
@@ -98,8 +96,8 @@ Tile tile_from_xml(pugi::xml_node& tile_node, int x, int y)
 		passable = true;
 		break;
 	}
-	
-	Tile tile{ type, sf::Vector2f{static_cast<float>(x), static_cast<float>(y)}, passable};
+
+	Tile tile{ type, sf::Vector2i{x * 64 ,y * 64}, passable };
 	return tile;
 }
 
@@ -114,7 +112,7 @@ void tile_to_xml(Tile& tile, pugi::xml_node& parent, bool save_position)
 		pugi::xml_node y = position.append_child("y");
 		y.append_child(pugi::node_pcdata).set_value(std::to_string(tile.position.y).c_str());
 	}
-
+	
 	pugi::xml_node type = tile_node.append_child("type");
 	type.append_child(pugi::node_pcdata).set_value(std::to_string(static_cast<int>(tile.type)).c_str());
 }
