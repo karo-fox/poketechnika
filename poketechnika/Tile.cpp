1d726f51ed0ed51d66028d3db1509ff6e215c84b
karo-fox <karolina.kozubik@gmail.com>
2023-06-04 12:41:31 +0200

    update saving and loading from xml

--- poketechnika/Tile.cpp
+++ poketechnika/Tile.cpp
@@ -5,11 +5,15 @@
 #include <map>
 #include <string>
 
+const std::string TEXTURES_DIR = "assets/textures";
+const sf::Vector2f SCALE{ 2, 2 };
+
 std::map<tileTypes, std::string> textures{
-	{tileTypes::GRASS, std::string{"assets/textures/grass.png"}},
-	{tileTypes::DIRT, std::string{"assets/textures/dirt.png"}},
-	{tileTypes::FLOOR, std::string{"assets/textures/floor.png"}},
-	{tileTypes::WALL, std::string{"assets/textures/wall.png"}},
+	{tileTypes::GRASS, std::string{TEXTURES_DIR + "/grass_tile.png"}},
+	{tileTypes::PAVEMENT, std::string{TEXTURES_DIR + "/pavement_tile.png"}},
+	{tileTypes::STREET, std::string{TEXTURES_DIR + "/street_tile.png"}},
+	{tileTypes::PATH, std::string{TEXTURES_DIR + "/path_tile.png"}},
+	{tileTypes::BUILDING, std::string{TEXTURES_DIR + "/building_tile.png"}},
 };
 
 Tile::Tile(const Tile& other) : tileTexture{ sf::Texture{} }, tileSprite{ sf::Sprite{} }
@@ -20,6 +24,7 @@ Tile::Tile(const Tile& other) : tileTexture{ sf::Texture{} }, tileSprite{ sf::Sp
 
 	if (tileTexture.loadFromFile(textures.at(type))) {
 		tileSprite.setTexture(tileTexture);
+		tileSprite.setScale(SCALE);
 	}
 	else {
 		throw Exception("Unable to load a texture from " + textures.at(type));
@@ -33,6 +38,7 @@ Tile::Tile(tileTypes type_, sf::Vector2f pos, bool passable_) : tileTexture{ sf:
 
 	if (tileTexture.loadFromFile(textures.at(type))) {
 		tileSprite.setTexture(tileTexture);
+		tileSprite.setScale(SCALE);
 	}
 	else {
 		throw Exception("Unable to load a texture from " + textures.at(type));
@@ -54,6 +60,7 @@ Tile& Tile::operator=(const Tile& other)
 
 	if (tileTexture.loadFromFile(textures.at(type))) {
 		tileSprite.setTexture(tileTexture);
+		tileSprite.setScale(SCALE);
 	}
 	else {
 		throw Exception("Unable to load a texture from " + textures.at(type));
@@ -67,28 +74,46 @@ sf::Sprite Tile::getSprite()
 	return tileSprite;
 }
 
-Tile tile_from_xml(pugi::xml_node& tile_node)
+Tile tile_from_xml(pugi::xml_node& tile_node, int x, int y)
 {
-	float x = std::stoi(tile_node.child("position").child("x").child_value()) * 64;
-	float y = std::stoi(tile_node.child("position").child("y").child_value()) * 64;
-	bool passable = std::stoi(tile_node.child("passable").child_value());
+	if (tile_node.child("position").hash_value()) {
+		x = std::stoi(tile_node.child("position").child("x").child_value());
+		y = std::stoi(tile_node.child("position").child("y").child_value());
+	}
+	x *= 64;
+	y *= 64;
+
 	tileTypes type = static_cast<tileTypes>(std::stoi(tile_node.child("type").child_value()));
-	Tile tile{ type, sf::Vector2f{x ,y}, passable };
+
+	bool passable{};
+	switch (type) {
+	case tileTypes::BUILDING:
+		passable = false;
+		break;
+	case tileTypes::GRASS:
+	case tileTypes::PATH:
+	case tileTypes::PAVEMENT:
+	case tileTypes::STREET:
+	default:
+		passable = true;
+		break;
+	}
+	
+	Tile tile{ type, sf::Vector2f{static_cast<float>(x), static_cast<float>(y)}, passable};
 	return tile;
 }
 
-void tile_to_xml(Tile& tile, pugi::xml_node& parent)
+void tile_to_xml(Tile& tile, pugi::xml_node& parent, bool save_position)
 {
 	pugi::xml_node tile_node = parent.append_child("tile");
 
-	pugi::xml_node position = tile_node.append_child("position");
-	pugi::xml_node x = position.append_child("x");
-	x.append_child(pugi::node_pcdata).set_value(std::to_string(tile.position.x).c_str());
-	pugi::xml_node y = position.append_child("y");
-	y.append_child(pugi::node_pcdata).set_value(std::to_string(tile.position.y).c_str());
-
-	pugi::xml_node passable = tile_node.append_child("passable");
-	passable.append_child(pugi::node_pcdata).set_value(std::to_string(tile.passable).c_str());
+	if (save_position) {
+		pugi::xml_node position = tile_node.append_child("position");
+		pugi::xml_node x = position.append_child("x");
+		x.append_child(pugi::node_pcdata).set_value(std::to_string(tile.position.x).c_str());
+		pugi::xml_node y = position.append_child("y");
+		y.append_child(pugi::node_pcdata).set_value(std::to_string(tile.position.y).c_str());
+	}
 
 	pugi::xml_node type = tile_node.append_child("type");
 	type.append_child(pugi::node_pcdata).set_value(std::to_string(static_cast<int>(tile.type)).c_str());
