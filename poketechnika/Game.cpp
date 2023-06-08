ce8f5700773d40feb909f20d89f4766696a19a90
karo-fox <karolina.kozubik@gmail.com>
2023-06-08 16:23:53 +0200

    fix

--- poketechnika/Game.cpp
+++ poketechnika/Game.cpp
@@ -11,6 +11,16 @@ Game::Game(int width, int height, bool fullscreen)
         {sf::Keyboard::K, Action::ChangeSceneToMenu},
        }, {} }, scenes{}
 {
+    if (fullscreen) {
+        width = sf::VideoMode::getDesktopMode().width;
+        height = sf::VideoMode::getDesktopMode().height;
+        window.create(
+            sf::VideoMode(width, height), "Poketechnika", sf::Style::Fullscreen
+        );
+    }
+    Camera::setCameraSize(width, height);
+    sm.set_renderer_scale();
+
     // Create all scenes used in game
     MenuScene menu_scene{};
     GameScene game_scene{};
@@ -23,16 +33,6 @@ Game::Game(int width, int height, bool fullscreen)
     // Set first scene to menu
     auto first_scene{ scenes.at(State::MAINMENU) };
     sm.set_scene(std::move(first_scene));
-
-    if (fullscreen) {
-        width = sf::VideoMode::getDesktopMode().width;
-        height = sf::VideoMode::getDesktopMode().height;
-        window.create(
-            sf::VideoMode(width, height), "Poketechnika", sf::Style::Fullscreen
-        );
-    }
-    Camera::setCameraSize(width, height);
-    sm.set_renderer_scale();
 }
 
 void Game::process_input() {
